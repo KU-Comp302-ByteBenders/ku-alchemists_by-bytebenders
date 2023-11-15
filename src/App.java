dce065d131c26b26b55392eec43c101c3273045e
Mustafa Er <91684719+ER-Mustafa@users.noreply.github.com>
2023-11-15 18:40:45 +0300

    initial frame. god speed boys

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Hello, World!");
+        frame.setTitle("KU ALCHEMIST");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
