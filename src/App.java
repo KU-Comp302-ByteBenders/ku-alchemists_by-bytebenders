cb88b30833abd7fc92472e8503224a64658c646b
ECU44 <91726557+ECU44@users.noreply.github.com>
2023-11-26 23:34:19 +0300

    Enes Can Ulutaş

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
-import ui.MainMenuJFrame;
-
-//GVARIS20
+//Enes Can Ulutaş 75632
+import javax.swing.JFrame;
 
 public class App {
+    String EnesCanUlutas;
     public static void main(String[] args) throws Exception {
-        MainMenuJFrame mainMenu = new MainMenuJFrame();
+        JFrame frame = new JFrame("Hello, World!");
+        frame.setTitle("KU ALCHEMIST");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
