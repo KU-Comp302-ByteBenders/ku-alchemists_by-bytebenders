e27a9787beab6556003b66c8d0b41122533bbd91
abahadircelik <acelik20@ku.edu.tr>
2023-11-26 23:52:43 +0300

    my-first-commit

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 //Enes Can Ulutaş 75632
+//Ali Bahadir Celik 75671
 import javax.swing.JFrame;
 
 public class App {
     String EnesCanUlutas;
+    String AliBahadirCelik;
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame("Hello, World!");
         frame.setTitle("KU ALCHEMIST");
