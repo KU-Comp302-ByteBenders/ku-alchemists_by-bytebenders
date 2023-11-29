ba0a885fab5eb5f2c0d47dd4e33129f44e300141
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-11-29 10:28:04 +0300

    init Log class

---
+++ src/Game/Log.java
@@ -0,0 +1,30 @@
+package Game;
+
+public class Log {
+    private Token[] tokens;
+    private String[] texts;
+
+    // no constructor as this is class is a Singleton
+
+    private void updateText(String newText) {}
+
+    private void displayText() {}
+
+    private String logStart() {}
+
+    private String logForageForIngredient() {}
+
+    private String logTransmuteIngredient() {}
+
+    private String logBuyArtifactCard(ArtifactCard artifactCard) {}
+
+    private String logUseArtifactCard(ArtifactCard artifactCard) {}
+
+    private String logMakeExperiment(Ingredient ing1, Ingredient ing2, String testSubject, String result) {}
+
+    private String logSellPotion(Potion potion, int price) {}
+
+    private String logPublishTheory(Ingredient ingredient, AlchemyMarker alchemyMarker) {}
+
+    private String logDebunkTheory(Ingredient ingredient, AlchemyMarker alchemyMarker, Token publisher, boolean success) {}
+}
