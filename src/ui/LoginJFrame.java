5c880e2047dfc45e191e31c47707324b00b8e35d
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-11-29 19:05:29 +0300

    add text fields and buttons

--- src/ui/LoginJFrame.java
+++ src/ui/LoginJFrame.java
@@ -8,18 +8,26 @@ public class LoginJFrame extends JFrame{
         // Create text fields
         JTextField textField1 = new JTextField(20);
         JTextField textField2 = new JTextField(20);
-        
+
         // Set the size of the frame
         this.setSize(1280, 720);
 
         // Create a drop-down menu
-        String[] options = {"Option 1", "Option 2", "Option 3"};
-        JComboBox<String> comboBox = new JComboBox<>(options);
+        String[] options1 = {"Avatar 1", "Avatar 2", "Avatar 3", "Avatar 4"};
+        JComboBox<String> comboBox1 = new JComboBox<>(options1);
+
+        String[] options2 = {"Avatar 1", "Avatar 2", "Avatar 3", "Avatar 4"};
+        JComboBox<String> comboBox2 = new JComboBox<>(options2);
+
+        // Create a "Start the Game" button
+        JButton startButton = new JButton("Start the Game");
 
         // Add text fields and drop-down menu to the frame
         this.add(textField1);
         this.add(textField2);
-        this.add(comboBox);
+        this.add(comboBox1);
+        this.add(comboBox2);
+        this.add(startButton);
 
         // Set the layout manager to null for absolute positioning
         this.setLayout(null);
@@ -27,6 +35,8 @@ public class LoginJFrame extends JFrame{
         // Set the position and size of the components
         textField1.setBounds(50, 50, 150, 20);
         textField2.setBounds(50, 80, 150, 20);
-        comboBox.setBounds(50, 110, 150, 20);
+        comboBox1.setBounds(50, 110, 150, 20);
+        comboBox2.setBounds(50, 140, 150, 20);
+        startButton.setBounds(50, 170, 150, 20); // Set the position and size of the button
     }
 }
