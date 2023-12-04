83d532b04b2f0cc469e89c12974f7b086703ba65
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-12-04 18:51:49 +0300

    add more theory fields and methods

--- src/game/Theory.java
+++ src/game/Theory.java
@@ -1,16 +1,41 @@
 package game;
-import java.util.ArrayList;
-
-//integerleri sametin commitinden sonra degistiricem
 
 public class Theory {
     private String theoryName;
-    private Ingredient[] theoryIngredients;
+    private Ingredient theoryIngredient;
+    private AlchemyMarker alchemyMarker;
+    private Token theoryOwner;
 
-    public Theory(Ingredient ingredient, AlchemyMarker alchemyMarker) {
-        this.theoryName = "DefaultTheoryName";
-        this.theoryIngredients = new Ingredient[0];
+    public Theory(Ingredient ingredient, AlchemyMarker alchemyMarker, Token token) {
+        this.theoryIngredient = ingredient;
+        this.alchemyMarker = alchemyMarker;
+        this.theoryOwner = token;
+        this.theoryName = String.format("Theory on %s", ingredient.getName());
+    }
 
-}
+    // Returns true if the theory is about the given ingredient
+    public boolean isAboutIngredient(Ingredient ingredient) {
+        return this.getTheoryIngredient().equals(ingredient);
+    }
 
+    // Returns true if the theory is marked with that Alchemical Marker
+    public boolean hasAlchemyMarker(AlchemyMarker alchemyMarker) {
+        return this.getAlchemyMarker().equals(alchemyMarker);
+    }
+
+    public Token getTheoryOwner() {
+        return theoryOwner;
+    }
+
+    public String getTheoryName() {
+        return theoryName;
+    }
+
+    public Ingredient getTheoryIngredient() {
+        return theoryIngredient;
+    }
+
+    public AlchemyMarker getAlchemyMarker() {
+        return alchemyMarker;
+    }
 }
