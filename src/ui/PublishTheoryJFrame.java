c4c9bdd6e47f29288c4b78853e6179f3cf485630
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-12-05 15:09:03 +0300

    add publish jframe

--- src/ui/PublishTheoryJFrame.java
+++ src/ui/PublishTheoryJFrame.java
@@ -1,33 +1,77 @@
 package ui;
+
+import game.Board;
+import game.Ingredient;
+import game.AlchemyMarker;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class PublishTheoryJFrame extends JFrame {
-    private JButton[] ingredientButtons = new JButton[8];
-    private JButton[] alchemyButtons = new JButton[8];
+    private JToggleButton[] ingredientButtons = new JToggleButton[8];
+    private JToggleButton[] alchemyButtons = new JToggleButton[8];
     private JButton confirmButton = new JButton("Confirm");
 
-    public PublishTheoryJFrame() {
+    private Board board;
+    private Ingredient selectedIngredient = null;
+    private AlchemyMarker selectedAlchemyMarker = null;
+
+    public PublishTheoryJFrame(Board board) {
         this.setSize(1280, 720);
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         this.setResizable(false);
+        
+        this.board = board;
 
         JPanel ingredientPanel = new JPanel(new GridLayout(1, 8));
         JPanel alchemyPanel = new JPanel(new GridLayout(1, 8));
         JPanel confirmPanel = new JPanel();
+        JPanel bottomPanel = new JPanel(new GridLayout(2, 1)); // Create a grid to display selected ingredient and alchemy marker.
+
+        // Create fields to display selected ingredient on bottom left.
+        JTextField selectedIngredientField = new JTextField();
+        selectedIngredientField.setEditable(false); // Make the text field non-editable
+        bottomPanel.add(selectedIngredientField, BorderLayout.WEST);
+
+        // Create fields to display selected alchemy marker on bottom left.
+        JTextField selectedMarkerField = new JTextField();
+        selectedMarkerField.setEditable(false); // Make the text field non-editable
+        bottomPanel.add(selectedMarkerField, BorderLayout.WEST);
 
         ButtonGroup ingredientGroup = new ButtonGroup();
         ButtonGroup alchemyGroup = new ButtonGroup();
 
         for (int i = 0; i < 8; i++) {
-            ingredientButtons[i] = new JButton();
-            ingredientButtons[i].setIcon(new ImageIcon("src/ui/utils/ingredient_" + (i + 1) + ".jpg"));
+            String imagePath = board.getStaticIngredients().get(i).getImagePath();
+            ingredientButtons[i] = new JToggleButton();
+            ingredientButtons[i].setIcon(new ImageIcon(imagePath));
             ingredientGroup.add(ingredientButtons[i]);
             ingredientPanel.add(ingredientButtons[i]);
 
-            alchemyButtons[i] = new JButton("Alchemy Marker" + (i + 1));
+            final int index = i; // Create a final copy of i for use in the listener
+            ingredientButtons[i].addItemListener(new ItemListener() {
+                public void itemStateChanged(ItemEvent e) {
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        // Get the selected ingredient from board
+                        selectedIngredient = board.getStaticIngredients().get(index);
+                        selectedIngredientField.setText("Selected Ingredient: " + selectedIngredient.getName());
+                    }
+                }
+            });
+
+            alchemyButtons[i] = new JToggleButton("Alchemy Marker" + (i + 1));
             //alchemyButtons[i].setIcon(new ImageIcon("path_to_alchemy_image_" + (i + 1)));
+            alchemyButtons[i].addItemListener(new ItemListener() {
+                public void itemStateChanged(ItemEvent e) {
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        // Get the selected marker from board
+                        selectedAlchemyMarker = board.getStaticAlchemyMarkers().get(index);
+                        selectedMarkerField.setText("Selected Marker: " + (index + 1));
+                    }
+                }
+            });
+
             alchemyGroup.add(alchemyButtons[i]);
             alchemyPanel.add(alchemyButtons[i]);
         }
@@ -42,6 +86,7 @@ public class PublishTheoryJFrame extends JFrame {
         this.add(ingredientPanel);
         this.add(alchemyPanel);
         this.add(confirmPanel);
+        this.add(bottomPanel);
         this.pack();
         this.setVisible(true);
     }
