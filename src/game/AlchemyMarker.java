0003ea8fed78e8b63fe9b4010b9e5419855863ba
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-12-06 11:15:18 +0300

    add getters and imagePath field

--- src/game/AlchemyMarker.java
+++ src/game/AlchemyMarker.java
@@ -6,16 +6,34 @@ public class AlchemyMarker {
     private Aspect aspect2;
     private Aspect aspect3;
 
+    private String imagePath;
 
-    public AlchemyMarker(Aspect aspect1, Aspect aspect2, Aspect aspect3) {
+    public AlchemyMarker(Aspect aspect1, Aspect aspect2, Aspect aspect3, String imagePath) {
         this.aspect1 = aspect1;
         this.aspect2 = aspect2;
         this.aspect3 = aspect3;
+
+        this.imagePath = imagePath;
     }
 
     public boolean isOnIngredient(Ingredient ingredient) {
 
         return false;
     }
-    
+
+    public String getPath() {
+        return imagePath;
+    }
+
+    public Aspect getAspect1() {
+        return aspect1;
+    }
+
+    public Aspect getAspect2() {
+        return aspect2;
+    }
+
+    public Aspect getAspect3() {
+        return aspect3;
+    }
 }
