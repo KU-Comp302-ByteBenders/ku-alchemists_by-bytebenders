2e0578ee20baf0c23f1316365e009939055b0143
Tolga Kaan Kantarcı <tkantarci21@ku.edu.tr>
2023-12-09 21:23:00 +0300

    add debunk theory frame

---
+++ src/ui/DebunkTheoryJFrame.java
@@ -0,0 +1,75 @@
+package ui;
+
+import game.Board;
+import game.Ingredient;
+import game.Theory;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DebunkTheoryJFrame extends JFrame {
+
+  public DebunkTheoryJFrame(BoardJFrame boardJFrame, Board board) {
+    this.setSize(1280, 720);
+    this.setResizable(false);
+    JPanel gridPanel = new JPanel(new GridLayout(2, 4));
+
+    for (int i = 0; i < 8; i++) {
+      JPanel cellPanel = new JPanel(new BorderLayout());
+
+      Ingredient ingredient = board.getStaticIngredients().get(i);
+      String imagePath = ingredient.getImagePath();
+      String theoryOwner = "No theory";
+      String theoryMarkerImage = null;
+
+      for (Theory theory : board.getTheories()) {
+        if (theory.isAboutIngredient(ingredient)) {
+          theoryOwner = theory.getTheoryOwner().getUsername();
+          theoryMarkerImage = theory.getAlchemyMarker().getPath();
+        }
+      }
+
+      JLabel textLabel = new JLabel(theoryOwner);
+      cellPanel.add(textLabel, BorderLayout.NORTH);
+
+      ImageIcon ingredientIcon = new ImageIcon(imagePath);
+      JLabel ingredientLabel = new JLabel(ingredientIcon);
+      cellPanel.add(ingredientLabel, BorderLayout.CENTER);
+
+      if (theoryMarkerImage != null) {
+        ImageIcon markerIcon = new ImageIcon(theoryMarkerImage);
+        JLabel markerLabel = new JLabel(markerIcon);
+
+        JButton debunkButton = new JButton("Debunk the theory on ingredient");
+        
+        JPanel southPanel = new JPanel(new BorderLayout());
+        southPanel.add(markerLabel, BorderLayout.NORTH);
+        southPanel.add(debunkButton, BorderLayout.SOUTH);
+
+        cellPanel.add(southPanel, BorderLayout.SOUTH);
+      }
+
+      gridPanel.add(cellPanel);
+    }
+
+    this.add(gridPanel);
+    this.pack();
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (screenSize.width - this.getWidth()) / 2;
+    int y = (screenSize.height - this.getHeight()) / 2;
+
+    // Set the frame location
+    this.setLocation(x, y);
+
+    // Make the frame visible
+    this.setVisible(true);
+  }
+}
