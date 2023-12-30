439b22ac379dc77e5d2523d3ce564bfbd06bd5e1
Gorkem-Varis <gvaris20@ku.edu.tr>
2023-12-30 16:52:26 +0300

    MakeExperimentTest is done

--- src/tests/MakeExperimentTest.java
+++ src/tests/MakeExperimentTest.java
@@ -1,36 +1,82 @@
 package tests;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import game.Token;
-import game.AlchemyMarker;
-import game.Aspect;
 import game.Board;
-import game.Ingredient;
 import game.Potion;
 
-import org.junit.jupiter.api.*;
 
 public class MakeExperimentTest {
-    @BeforeEach
-    public void setUp() {
-            
-    }
-
+    Token token = new Token("User1", "Avatar1", "Token1");
+    Board board = new Board("User1", "User2", "Avatar1", "Avatar2");
     
-    @Test
-    public void testMakeExperiment() {
-        Token token = new Token("User1", "Avatar1", "Token1");
-        Board board = new Board("User1", "User2", "Avatar1", "Avatar2");
+    //creating ingredients for the token
+    @Before
+    public void setUp() {    
         board.createIngredients();
         token.addIngredient(board.getIngredientByName("feather"));
         token.addIngredient(board.getIngredientByName("dragon fruit"));
         token.addIngredient(board.getIngredientByName("ginger"));
         token.addIngredient(board.getIngredientByName("obsidian"));    
-        
-        Potion potion1 = token.makeExperiment("feather", "dragon fruit", false);
-        Potion potion2 = new Potion("red", token.getIngredients().get(0), token.getIngredients().get(1), "+", "not negative potion");
-        assertEquals(potion1.getName(), potion2.getName());
+    }
 
+    //controlling the make experiment result with the expected result
+    //if the name's are equal, the test is passed
+    @Test
+    public void testMakeExperiment() {
+         
+        Potion potion1 = token.makeExperiment("feather", "dragon fruit", false);
+        Potion potion2 = new Potion("red", token.getIngredients().get(0)
+                        ,token.getIngredients().get(1), "+", "not negative potion");
+        
+        assertEquals(potion1.getName(), potion2.getName());
+    }
+
+    //if the color's are equal, the test is passed
+    @Test
+    public void testMakeExperiment2() {
+         
+        Potion potion1 = token.makeExperiment("ginger", "obsidian", true);
+        Potion potion2 = new Potion("Yellow", token.getIngredients().get(0)
+                        ,token.getIngredients().get(1), "+", "not negative potion");
+        
+        assertEquals(potion1.getPotionColor(), potion2.getPotionColor());
+    }
+
+    //if the guarantee's are equal, the test is passed
+    @Test
+    public void testMakeExperiment3() {
+         
+        Potion potion1 = token.makeExperiment("feather", "obsidian", true);
+        Potion potion2 = new Potion("Yellow", token.getIngredients().get(0)
+                        ,token.getIngredients().get(1), "-", "negative potion");
+
+        assertEquals(potion1.getGuarantee(), potion2.getGuarantee());
+    }
+
+    //if the name's are not equal, the test is passed but name should be 0 in here
+    @Test
+    public void testMakeExperiment4() {
+         
+        Potion potion1 = token.makeExperiment("ginger", "ginger", false);
+        Potion potion2 = new Potion("transparent", token.getIngredients().get(0)
+                        ,token.getIngredients().get(1), "+", "not negative potion");
+        
+        assertFalse(potion1.getName().equals(potion2.getName()));
+    }
+
+    //if the color's are equal, the test is passed
+    @Test
+    public void testMakeExperiment5() {
+         
+        Potion potion1 = token.makeExperiment("feather", "ginger", false);
+        Potion potion2 = new Potion("transparent", token.getIngredients().get(0)
+                        ,token.getIngredients().get(1), "0", "not negative potion");
+        
+        assertTrue(potion1.getPotionColor().equals(potion2.getPotionColor()));
     }
 }
