f108a1642b395f273cc45b66a4f75274d1abf342
Mustafa Er <91684719+ER-Mustafa@users.noreply.github.com>
2023-12-30 21:32:02 +0300

    added remaining tests for end gamer.

--- src/game/EndGamer.java
+++ src/game/EndGamer.java
@@ -9,6 +9,9 @@ public class EndGamer {
   private int winner; // -1 for tie, 0 for token1, 1 for token2
   private int token1Score;
   private int token2Score;
+  public static final int TIE = -1;
+  public static final int TOKEN1 = 0;
+  public static final int TOKEN2 = 1;
 
   public EndGamer(Board board) {
     tokens = board.getTokens();
@@ -42,11 +45,17 @@ public class EndGamer {
     int token1Reputation = token1.getReputation();
     int token2Reputation = token2.getReputation();
 
+    System.out.println("token1Reputation: " + token1Reputation);
+    System.out.println("token2Reputation: " + token2Reputation);
+
     int token1ArtifactCardNumber = token1.getArtifactCards().size();
     int token2ArtifactCardNumber = token2.getArtifactCards().size();
 
     int token1GoldAmount = token1.getGoldBalance();
     int token2GoldAmount = token2.getGoldBalance();
+
+    System.out.println("token1GoldAmount: " + token1GoldAmount);
+    System.out.println("token2GoldAmount: " + token2GoldAmount);
 
     token1GoldAmount += token1ArtifactCardNumber * 2;
     token2GoldAmount += token2ArtifactCardNumber * 2;
@@ -56,6 +65,9 @@ public class EndGamer {
 
     int token1Score = token1Reputation * 10 + scoreFromGoldToken1;
     int token2Score = token2Reputation * 10 + scoreFromGoldToken2;
+
+    System.out.println("token1Score: " + token1Score);
+    System.out.println("token2Score: " + token2Score);
 
     if (token1Score > token2Score) {
       winner = 0;
--- src/tests/EndGamerTest.java
+++ src/tests/EndGamerTest.java
@@ -4,20 +4,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import game.Board;
 import game.EndGamer;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EndGamerTest {
 
+  String username1;
+  String username2;
+  String avatar1;
+  String avatar2;
+  Board board;
+  EndGamer endGamer;
+
+  @BeforeEach
+  public void setUp() {
+    this.username1 = "User1";
+    this.username2 = "User2";
+    this.avatar1 = "Avatar1";
+    this.avatar2 = "Avatar2";
+    this.board = new Board(username1, username2, avatar1, avatar2);
+  }
+
+  @AfterEach
+  public void tearDown() {
+    username1 = null;
+    username2 = null;
+    avatar1 = null;
+    avatar2 = null;
+    board = null;
+    endGamer = null;
+  }
+
   @Test
-  public void testEndGame() {
-    String username1 = "User1";
-    String username2 = "User2";
-    String avatar1 = "Avatar1";
-    String avatar2 = "Avatar2";
-    Board board = new Board(username1, username2, avatar1, avatar2);
+  public void testTie() {
+    endGamer = new EndGamer(board);
+    assertEquals(EndGamer.TIE, endGamer.getWinner()); // -1 for tie, 0 for token1, 1 for token2
+  }
 
-    EndGamer endGamer = new EndGamer(board);
+  @Test
+  public void testToken1WinWithGold() {
+    board.getTokens().get(0).addGold(10);
+    endGamer = new EndGamer(board);
+    assertEquals(EndGamer.TOKEN1, endGamer.getWinner());
+  }
 
-    assertEquals(-1, endGamer.getWinner()); // -1 for tie, 0 for token1, 1 for token2
+  @Test
+  public void testToken2WinWithGold() {
+    board.getTokens().get(1).addGold(10);
+    endGamer = new EndGamer(board);
+    assertEquals(EndGamer.TOKEN2, endGamer.getWinner());
+  }
+
+  @Test
+  public void testToken1WinWithReputation() {
+    board.getTokens().get(0).addReputation(5);
+    endGamer = new EndGamer(board);
+    assertEquals(EndGamer.TOKEN1, endGamer.getWinner());
+  }
+
+  @Test
+  public void testToken2WinWithReputation() {
+    board.getTokens().get(1).addReputation(5);
+    endGamer = new EndGamer(board);
+    assertEquals(EndGamer.TOKEN2, endGamer.getWinner());
   }
 }
