b28d31d1b1032b713545aaa3c814d480aeacbb5d
Gorkem-Varis <gvaris20@ku.edu.tr>
2024-01-05 15:35:38 +0300

    board test fix

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import game.AlchemyMarker;
 import game.Board;
+import game.Ingredient;
+import game.Theory;
 import game.Token;
 import org.junit.jupiter.api.*;
 
@@ -75,19 +78,106 @@ class BoardTest {
     }
 
     @Test
-    void testInitialGameState() {
+    void testPublishTheoryWithNullIngredient() {
         // parameters for testing
         String username1 = "User1";
         String username2 = "User2";
         String avatar1 = "Avatar1";
         String avatar2 = "Avatar2";
 
-        // Create a board with the parameters
+        // create a board with the parameters
         Board board = new Board(username1, username2, avatar1, avatar2);
+        Token token = board.getTokens().get(0);
+        AlchemyMarker alchemyMarker = board.getStaticAlchemyMarkers().get(0);
 
-        // Check if the state object is not null
-        assertNotNull(board.getState());
+        Exception exception = assertThrows(Exception.class, () -> {
+            board.publishTheory(null, alchemyMarker, token);
+        });
+
+        assertEquals("Please select an ingredient and an alchemy marker!", exception.getMessage());
     }
 
+    @Test
+    void testPublishTheoryWithNullAlchemyMarker() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
 
+        // create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+        Token token = board.getTokens().get(0);
+        Ingredient ingredient = board.getStaticIngredients().get(0);
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            board.publishTheory(ingredient, null, token);
+        });
+
+        assertEquals("Please select an ingredient and an alchemy marker!", exception.getMessage());
+    }
+
+    @Test
+    void testPublishTheoryWithInsufficientGold() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
+
+        // create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+        Token token = board.getTokens().get(0);
+        token.decreaseGold(10); // decrease the gold balance to 0
+        AlchemyMarker alchemyMarker = board.getStaticAlchemyMarkers().get(0);
+        Ingredient ingredient = board.getStaticIngredients().get(0);
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            board.publishTheory(ingredient, alchemyMarker, token);
+        });
+
+        assertEquals("Not enough gold!", exception.getMessage());
+    }
+
+    @Test
+    void testPublishTheoryWithExistingTheory() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
+
+        // create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+        Token token = board.getTokens().get(0);
+        AlchemyMarker alchemyMarker = board.getStaticAlchemyMarkers().get(0);
+        Ingredient ingredient = board.getStaticIngredients().get(0);
+        Theory existingTheory = new Theory(ingredient, alchemyMarker, token);
+        board.getTheories().add(existingTheory);
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            board.publishTheory(ingredient, alchemyMarker, token);
+        });
+
+        assertEquals("A theory on this ingredient already exists!", exception.getMessage());
+    }
+
+    @Test
+    void testPublishTheorySuccessfully() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
+
+        // create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+        Token token = board.getTokens().get(0);
+        AlchemyMarker alchemyMarker = board.getStaticAlchemyMarkers().get(0);
+        Ingredient ingredient = board.getStaticIngredients().get(0);
+
+        assertDoesNotThrow(() -> {
+            board.publishTheory(ingredient, alchemyMarker, token);
+        });
+    }
 }
