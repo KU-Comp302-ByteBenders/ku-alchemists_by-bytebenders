5ff7122bf017f3bca3e27b44a1104d76cc287a9f
s-bbr <sbiber20@ku.edu.tr>
2024-01-06 16:15:18 +0300

    who deleted my tests

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -60,8 +60,89 @@ class BoardTest {
         }
     }
 
+    @Test
+    void testAddIngredientToNonEmptyBoard() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
 
+        // I create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
 
+        board.addIngredient();
+        int initialSize = board.getIngredients().size();
+        board.addIngredient();
+        assertTrue(board.getIngredients().size() > initialSize, "Adding ingredients should increase the total number of ingredients");
+    }
+
+     // Glass-box tests
+    @Test
+    void testAddIngredientDistribution() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
+
+        // I create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+        board.addIngredient();
+
+        int initialSize = 0;
+        for (Token token : board.getTokens()) {
+            initialSize += token.getIngredients().size();
+        }
+
+        board.addIngredient();
+        int finalSize = 0;
+        for (Token token : board.getTokens()) {
+            finalSize += token.getIngredients().size();
+        }
+
+        assertTrue(finalSize > initialSize, "Adding ingredients should increase the total number of ingredients in tokens");
+    }
+
+    @Test
+    void testAddIngredientRemovalFromBoard() {
+        // parameters for testing
+        String username1 = "User1";
+        String username2 = "User2";
+        String avatar1 = "Avatar1";
+        String avatar2 = "Avatar2";
+
+        // I create a board with the parameters
+        Board board = new Board(username1, username2, avatar1, avatar2);
+
+        board.addIngredient();
+        int initialSize = board.getIngredients().size();
+        board.addIngredient();
+        assertTrue(board.getIngredients().size() > initialSize, "Adding ingredients should increase the total number of ingredients on the board");
+    }
+
+    @Test
+void testAddIngredientDistributionAndEmptyBoard() {
+    // parameters for testing
+    String username1 = "User1";
+    String username2 = "User2";
+    String avatar1 = "Avatar1";
+    String avatar2 = "Avatar2";
+
+    // I create a board with the parameters
+    Board board = new Board(username1, username2, avatar1, avatar2);
+
+    // Check that each token has exactly 2 ingredients
+    for (Token token : board.getTokens()) {
+        assertEquals(2, token.getIngredients().size(), "Each token should have 4 ingredients after the first call addIngredient().");
+    }
+
+    board.addIngredient();
+    // Check that each token gets 2 more ingredients when addIngredient() is called
+    for (Token token : board.getTokens()) {
+        assertEquals(4, token.getIngredients().size(), "Each token should have 4 ingredients after the first call addIngredient().");
+    }
+}
     @Test
     void testArtifactsCards() {
         // parameters for testing
